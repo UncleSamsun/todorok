12d607187a6cf1d81da3a99c03347afd37b0fac3
UncleSamsun <alswns5620@naver.com>
2026-08-31 14:46:43 +0900

    feat(activity): 활동 서비스 기반 구성

---
+++ services/activity-service/src/test/java/io/todorok/activity/ActivityApplicationTest.java
@@ -0,0 +1,12 @@
+package io.todorok.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
+
